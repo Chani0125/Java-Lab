c3f095fcf2ea7eebb8c0d02f9c830773f8605a2f
Mincho_Park <ap25ap25@naver.com>
2022-09-22 13:11:16 +0900

    Create 20223082-Lab04

---
+++ 20223082-Lab04/src/DoubleTester.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class DoubleTester 
+{ 
+    public static void main(String args[]) 
+    { 
+       double probability = 8.70; 
+       int percentage = (int) Math.round(100 * probability); 
+       System.out.println(percentage);
+    } 
+}
