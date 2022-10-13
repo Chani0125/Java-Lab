f03306ab9f2ec32d73a3ecbbffda5c58c796d3da
Mincho_Park <ap25ap25@naver.com>
2022-10-13 17:22:30 +0900

    Change

---
+++ Test/src/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		boolean condition1 = false;
+		boolean condition2 = false;
+		
+		if (condition1)
+			if (condition2)
+				System.out.println("1");
+		else System.out.println("2");
+
+	}
+
+}
